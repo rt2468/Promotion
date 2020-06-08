a1c9cc7ccadf49cf9d7edd74fa609258725ae4a2
RobinThomas <robin.thomas@gaf.com>
2020-06-08 09:10:05 +0530

    Added Tests for Promotion Engine

---
+++ PromotionEngine/Promotions/PromotionEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PromotionEngine.Entities;
+
+namespace PromotionEngine.Promotions
+{
+	public static class PromotionEngineClient
+	{
+
+		public static Cart Apply(Cart cart)
+		{
+			return new Cart();
+		}
+	}
+}
---
+++ PromotionEngineTests/PromotionTests/PromotionEngineTests.cs
@@ -0,0 +1,50 @@
+using PromotionEngine.Entities;
+using Xunit;
+using PromotionEngine.Promotions;
+
+namespace PromotionEngineTests.PromotionTests
+{
+	public class PromotionEngineTests
+	{
+
+		[Fact]
+		public void PromotionEngine_WhenNoPromotion_ReturnsTotalAmount()
+		{
+			var cart = new Cart();
+			cart.AddItem("A", 50, 1);
+			cart.AddItem("B", 30, 1);
+			cart.AddItem("C", 20, 1);
+
+			cart = PromotionEngineClient.Apply(cart);
+
+			Assert.Equal(100, cart.GetTotalAmount());
+		}
+
+		[Fact]
+		public void PromotionEngine_NQuantitiesOfPromotionalItemsAreAdded_DiscountShouldBeApplied()
+		{
+			var cart = new Cart();
+			cart.AddItem("A", 50, 5);
+			cart.AddItem("B", 30, 5);
+			cart.AddItem("C", 20, 1);
+
+			cart = PromotionEngineClient.Apply(cart);
+
+			Assert.Equal(370, cart.GetTotalAmount());
+		}
+
+		[Fact]
+		public void PromotionEngine_CombinationPromotionalItemsAreAdded_DiscountShouldBeApplied()
+		{
+			var cart = new Cart();
+			cart.AddItem("A", 50, 3);
+			cart.AddItem("B", 30, 5);
+			cart.AddItem("C", 20, 1);
+			cart.AddItem("D", 15, 1);
+
+			cart = PromotionEngineClient.Apply(cart);
+
+			Assert.Equal(280, cart.GetTotalAmount());
+		}
+	}
+}
