17115abe00b6b6b02de3a90963efb58481819159
RobinThomas <robin.thomas@gaf.com>
2020-06-08 08:54:18 +0530

    Added UnitTests for Cart

---
+++ PromotionEngine/Entities/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Entities
+{
+	public class Cart
+	{
+		private readonly List<CartItem> _items = new List<CartItem>();
+		public IReadOnlyCollection<CartItem> Items => _items.AsReadOnly();
+
+		public void AddItem(string sku, decimal testUnitPrice, int testQuantity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ PromotionEngineTests/CartTests/CartAddItemTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using PromotionEngine.Entities;
+using Xunit;
+
+namespace PromotionEngineTests.CartTests
+{
+	public class CartAddItemTests
+	{
+		private readonly string _sku = "A";
+		private readonly decimal _testUnitPrice = 45m;
+		private readonly int _testQuantity = 2;
+
+
+		[Fact]
+		public void NewCartItem_ShouldBeAdded_IfItemNotPresent()
+		{
+			var cart = new Cart();
+			cart.AddItem(_sku, _testUnitPrice, _testQuantity);
+
+			var firstItem = cart.Items.Single();
+			Assert.Equal(_sku, firstItem.Sku);
+			Assert.Equal(_testUnitPrice, firstItem.UnitPrice);
+			Assert.Equal(_testQuantity, firstItem.Quantity);
+		}
+
+		[Fact]
+		public void QuantityofItem_ShouldBe_Incremented_IfCartItemExists()
+		{
+			var cart = new Cart();
+			cart.AddItem(_sku, _testUnitPrice, _testQuantity);
+			cart.AddItem(_sku, _testUnitPrice, _testQuantity);
+
+			var firstItem = cart.Items.Single();
+			Assert.Equal(_testQuantity * 2, firstItem.Quantity);
+		}
+	}
+}
